50cf69dcb3361723a115725b5154ff8dc6d04d9c
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2023-11-08 12:20:34 +0700

    Создание единой точки входа

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,9 +1,12 @@
-from .models import Snippet
 from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import SnippetSerializer, UserSerializer
-from rest_framework import permissions
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
+from .serializers import SnippetSerializer, UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
    queryset = Snippet.objects.all()
@@ -28,4 +31,13 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
    queryset = User.objects.all()
    serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+   return Response({
+       'users': reverse('user-list', request=request, format=format),
+       'snippets': reverse('snippet-list', request=request, format=format)
+   })
+
 
