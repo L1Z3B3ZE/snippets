5c37b4569a29f2bddb60ade6428c43f5fe86389f
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2023-11-08 12:51:50 +0700

    Явная привязка ViewSets к URL-адресам

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,6 +1,31 @@
 from django.urls import path, include
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import renderers
+from .views import SnippetViewSet, UserViewSet
+
+
+snippet_list = SnippetViewSet.as_view({
+   'get': 'list',
+   'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+   'get': 'retrieve',
+   'put': 'update',
+   'patch': 'partial_update',
+   'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+   'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+   'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+   'get': 'retrieve'
+})
+
+
 
 urlpatterns = [
    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
